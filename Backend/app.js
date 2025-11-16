c633412b934210025ac97c9a28cdbe6767cb9705
Arjun8400 <youji099@gmail.com>
2025-11-16 12:06:01 +0530

    api

--- Backend/app.js
+++ Backend/app.js
@@ -12,7 +12,10 @@ app.use(cors())
 app.use(express.static('public'))
 app.use(express.json()) //! JSON data ko allow kiye
 app.use("/api", apiRouter)
-let port = process.env.port || 5000
+app.get("/", (req, res)=>{
+    res.send("API is running...")
+})
+let port = process.env.PORT || 5000
 app.listen(port, ()=>{
     console.log(`Server Running port ${port}`)
 })
